6ad47e337d8ad430d31a4c1b417e22338bbbd18e
Patrick <pscale01@gmail.com>
2022-04-27 13:12:50 -0700

    Add solarpunk art

--- src/issues/02-solarpunk/pages/5.sh
+++ src/issues/02-solarpunk/pages/5.sh
@@ -1,6 +1,6 @@
-page_title="Solarpunk artists"
-page_img_name=Everyone
+page_title="Bay area solarpunks"
+page_img_name=Benjamin
 page_img_ext=jpg
 page_img_alt=$page_title
-page_content="Thanks for making the solarpunk drink and draw so much fun, everyone!"
-page_name=Thanks
+page_content="by Benjamin. We had a little meetup crossover w/ <a href=\"https://www.meetup.com/bay-area-solarpunks/\">Bay area solarpunks!</a>"
+page_name=Benjamin
---
+++ src/issues/02-solarpunk/pages/6.sh
@@ -0,0 +1,6 @@
+page_title="Solarpunk artists"
+page_img_name=Everyone
+page_img_ext=jpg
+page_img_alt=$page_title
+page_content="Thanks for making the solarpunk drink and draw so much fun, everyone!"
+page_name=Thanks
